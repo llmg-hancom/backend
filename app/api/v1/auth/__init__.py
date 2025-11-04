0187bbedfde801e10a529d3eccb995f535e39e05
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-04 16:40:42 +0900

    구글 로그인 관련 라우터 추가

--- app/api/v1/auth/__init__.py
+++ app/api/v1/auth/__init__.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 # routes
 from .register import router as register_router
 from .login import router as login_router
+from .google import router as google_router
 
 router = APIRouter(prefix="/auth", tags=["인증"])
 router.include_router(register_router)
 router.include_router(login_router)
+router.include_router(google_router)
