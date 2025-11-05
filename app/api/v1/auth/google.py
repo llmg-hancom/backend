8e80d21d38491d598efb6fa79a283fec3feb399e
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-05 15:14:42 +0900

    구글 로그인 기능 일부 구현

--- app/api/v1/auth/google.py
+++ app/api/v1/auth/google.py
@@ -1,13 +1,54 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from urllib.parse import urlencode
+
+from fastapi.responses import RedirectResponse
+from core.config import settings
+from schemas.auth import LoginWithGoogleCallbackParam
+from sqlmodel import Session
+from db.session import get_db
+from services.auth.google import (
+    login_with_google_callback as google_callback_service,
+)
+
 
 router = APIRouter()
 
 
 @router.get("/google", summary="구글 계정으로 로그인")
 async def login_with_google():
-    pass
+    """
+    구글 로그인 페이지 링크를 반환합니다.
+    """
+    param = {
+        "client_id": settings.GOOGLE_CLIENT_ID,
+        "redirect_uri": settings.GOOGLE_REDIRECT_URI,
+        "response_type": "code",
+        "scope": "openid email profile",
+    }
+
+    url = f"https://accounts.google.com/o/oauth2/v2/auth?{urlencode(param)}"
+    return {"url": url}
 
 
 @router.get("/google/callback", summary="구글 계정 로그인 콜백")
-async def login_with_google_callback():
-    pass
+def login_with_google_callback(
+    param: Annotated[LoginWithGoogleCallbackParam, Depends()],
+    db: Annotated[Session, Depends(get_db)],
+) -> RedirectResponse:
+    """
+    구글로부터 전달받은 코드를 사용하여 사용자의 이름과 이메일을 가져옵니다.
+
+    만약 사용자가 이미 가입되어 있다면, 로그인을 진행합니다.
+    그렇지 않다면 Google로부터 받은 이름과 이메일로 User 테이블에 추가합니다.
+
+    로그인이 완료되면 프론트엔드의 메인 페이지로 리다이렉트됩니다.
+
+    TODO: 응답을 어떻게 프론트엔드로 전달할지는 아직 결정하지 못함
+    """
+    login = google_callback_service(param.code, db)
+
+    # 프론트엔드로 리다이렉트
+    response = RedirectResponse(url=settings.FRONTEND_URL, status_code=303)
+
+    return response
--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,13 @@
 from sqlalchemy import UniqueConstraint
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 from sqlalchemy import Column, DateTime
 from pydantic import EmailStr
 from datetime import datetime, timezone
 from uuid import UUID, uuid4
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.social_account import SocialAccount
 
 
 class UserBase(SQLModel):
@@ -15,12 +19,18 @@ class UserBase(SQLModel):
 
 class User(UserBase, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True, description="유저 ID")
-    hashed_password: str = Field(description="해시된 비밀번호")
+    hashed_password: str | None = Field(
+        description="해시된 비밀번호. 소셜 로그인일 경우 NULL이 들어감"
+    )
     is_active: bool = Field(default=True, description="활성화 여부")
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(tz=timezone.utc),
         sa_column=Column(DateTime(timezone=True), nullable=False),
         description="유저 생성 시간",
+    )
+
+    social_accounts: list["SocialAccount"] = Relationship(
+        back_populates="user",
     )
 
 
