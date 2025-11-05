e06cd5e290112281a603874c736c24bc8d84f73c
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-05 15:05:36 +0900

    JSON을 반환할 때 `Content-Type` 헤더를 `application/json; charset=utf-8`로 바꿔서 반환하도록 변경

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from core.config import settings
 from api import router
 from api.exceptions import register_exception_handlers
+from utils.charset import CharsetMiddleware
 
 app = FastAPI()
 
 app.include_router(router)
+app.add_middleware(CharsetMiddleware)
 register_exception_handlers(app)
