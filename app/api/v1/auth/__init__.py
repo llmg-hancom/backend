8f91d0c1d0abea8a1a7776bd71a79d4c2d0c70a8
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-06 13:26:38 +0900

    `POST /v1/auth/refresh` 엔드포인트 추가 (기능 미구현)

--- app/api/v1/auth/__init__.py
+++ app/api/v1/auth/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
-
-# routes
-from .register import router as register_router
-from .login import router as login_router
+# routers
 from .google import router as google_router
+from .login import router as login_router
+from .refresh import router as refresh_router
+from .register import router as register_router
+
 
 router = APIRouter(prefix="/auth", tags=["인증"])
 router.include_router(register_router)
 router.include_router(login_router)
 router.include_router(google_router)
+router.include_router(refresh_router)
---
+++ app/api/v1/auth/refresh.py
@@ -0,0 +1,15 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from db.session import get_db
+from schemas.auth import TokenRefreshRequest
+
+
+router = APIRouter()
+
+
+@router.post("/refresh")
+def refresh_access_token(
+    body: TokenRefreshRequest, db: Annotated[Session, Depends(get_db)]
+):
+    raise NotImplementedError()
