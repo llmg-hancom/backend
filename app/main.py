fcad8733b59bf363d53c3737590fcf2067280c24
hoyneed <hoyneed@gmail.com>
2025-11-07 15:07:48 +0900

    [auth] use cookie for auth 회원 인증 방식 cookie 방식으로 변경

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from api import router
 from api.exceptions import register_exception_handlers
@@ -10,4 +11,11 @@ app = FastAPI()
 
 app.include_router(router)
 app.add_middleware(CharsetMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[settings.FRONTEND_URL], # ⬅️ "*" 대신 명시적 URL 사용
+    allow_credentials=True, # ⬅️ ⭐️ 필수!
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 register_exception_handlers(app)
