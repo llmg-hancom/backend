931519cc6ce16d770a6a98deb5c7e5f7ad3d14b8
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-10 11:11:55 +0900

    사용자가 소속된 그룹 목록 엔드포인트 (`GET /v1/groups`) 추가

--- app/api/v1/groups/__init__.py
+++ app/api/v1/groups/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .create_group import router as create_group_router
+from .my_group import router as my_group_router
 
 
 router = APIRouter(prefix="/groups", tags=["Groups"])
 
 router.include_router(create_group_router)
+router.include_router(my_group_router)
