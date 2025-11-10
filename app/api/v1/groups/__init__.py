8ab1589514706ad98f63532007e4ff79e9ee73c5
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-10 13:56:17 +0900

    그룹 멤버 탈퇴 엔드포인트(`DELETE /groups/{group_id}/members/{member_id}`) 추가 (미구현)

--- app/api/v1/groups/__init__.py
+++ app/api/v1/groups/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .create_group import router as create_group_router
 from .group_invite import router as invite_router
 from .my_group import router as my_group_router
+from .remove_group_member import router as remove_group_member_router
 
 
 router = APIRouter(prefix="/groups", tags=["Groups"])
@@ -10,3 +11,4 @@ router = APIRouter(prefix="/groups", tags=["Groups"])
 router.include_router(create_group_router)
 router.include_router(my_group_router)
 router.include_router(invite_router)
+router.include_router(remove_group_member_router)
