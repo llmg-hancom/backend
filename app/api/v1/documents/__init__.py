5f48e5757910df15e73a64b3e290d2190cbf2393
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-12 10:49:28 +0900

    fix typo
    문서 관련 API 엔드포인트의 경로에서 `documents` 대신 `document`로 되어 있던 코드 수정

--- app/api/v1/documents/__init__.py
+++ app/api/v1/documents/__init__.py
@@ -4,7 +4,7 @@ from .my_docs import router as my_docs_router
 from .upload import router as upload_router
 
 
-router = APIRouter(prefix="/document", tags=["문서"])
+router = APIRouter(prefix="/documents", tags=["문서"])
 
 router.include_router(upload_router)
 router.include_router(my_docs_router)
