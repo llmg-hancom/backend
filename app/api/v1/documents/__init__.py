5e3ae28a427cb2bd339df8ecc375f8ae2b3dd729
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-12 14:02:43 +0900

    문서 삭제 API 엔드포인트 구현
    `DELETE /v1/documents/{document_id}`

--- app/api/v1/documents/__init__.py
+++ app/api/v1/documents/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .delete_docs import router as delete_docs_router
 from .get_docs_info import router as get_docs_info_router
 from .my_docs import router as my_docs_router
 from .upload import router as upload_router
@@ -10,3 +11,4 @@ router = APIRouter(prefix="/documents", tags=["문서"])
 router.include_router(upload_router)
 router.include_router(my_docs_router)
 router.include_router(get_docs_info_router)
+router.include_router(delete_docs_router)
