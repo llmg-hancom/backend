a4d8daaf1268238412d839edb08beab02f97f55d
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-12 16:55:15 +0900

    사용자 정보 변경 API 엔드포인트 작성 등
    - `PATCH /v1/users/me` - /users/me 부분 라우터 구조 변경

--- app/api/v1/users/__init__.py
+++ app/api/v1/users/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
 # routes
-from .me.info import router as my_info_router
+from .me import router as me_router
 
 
 router = APIRouter(prefix="/users", tags=["사용자"])
-router.include_router(my_info_router)
+router.include_router(me_router)
---
+++ app/api/v1/users/me/edit_info.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Body, Depends, status
+from sqlmodel import Session
+
+from db.session import get_db
+from models.user import User, UserEdit, UserRead
+from utils.auth import get_current_user
+
+
+router = APIRouter()
+
+@router.patch(
+    path="/",
+    status_code=status.HTTP_200_OK,
+    summary="유저 정보 변경"
+)
+def edit_user_info(
+    user: Annotated[User, Depends(get_current_user)],
+    user_edit: Annotated[UserEdit, Body()],
+    session: Annotated[Session, Depends(get_db)],
+) -> UserRead:
+    """
+    사용자의 정보를 변경합니다.
+    """
+
+    user_edit_data = user_edit.model_dump(exclude_unset=True)
+    user = user.sqlmodel_update(user_edit_data)
+
+    _ = session.merge(user)
+    session.commit()
+
+    return UserRead.model_validate(user)
