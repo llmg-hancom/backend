898737b2112c3b2b9c0a174d0071b089ef62ec04
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-12 10:48:27 +0900

    내 문서 조회 엔드포인트 구현
    `GET /v1/documents`

--- app/api/v1/documents/__init__.py
+++ app/api/v1/documents/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from .my_docs import router as my_docs_router
 from .upload import router as upload_router
 
 
 router = APIRouter(prefix="/document", tags=["문서"])
 
 router.include_router(upload_router)
+router.include_router(my_docs_router)
