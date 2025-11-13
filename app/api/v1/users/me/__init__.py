fd44cd5c9deab3f23586c8b44ab86a71f3249066
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-13 13:46:14 +0900

    사용자의 문서 목록 조회 API 변경
    사용자의 문서 목록을 조회하는 기존 API 엔드포인트를 deprecated시키고, 새로 `GET
    /users/me/documents` 엔드포인트 추가

--- app/api/v1/users/me/__init__.py
+++ app/api/v1/users/me/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .change_password import router as change_password_router
 from .edit_info import router as edit_info_router
 from .info import router as info_router
+from .my_documents import router as my_documents_router
 from .my_groups import router as my_groups_router
 
 
@@ -12,3 +13,4 @@ router.include_router(edit_info_router)
 router.include_router(info_router)
 router.include_router(change_password_router)
 router.include_router(my_groups_router)
+router.include_router(my_documents_router)
