84c4fc3f511b00971703beb169be8b9da326c878
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-20 13:24:02 +0900

    내 문서 조회 엔드포인트(`/v1/users/me/documents`)에 페이지네이션 기능 추가

--- app/api/v1/users/me/my_documents.py
+++ app/api/v1/users/me/my_documents.py
@@ -1,9 +1,13 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Security
+from fastapi import APIRouter, Depends, Query, Security
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from db.session import get_async_db
 from models.document import DocumentRead
 from models.user import User
+from schemas.pagination import PaginationParams, PaginationResponse
+from services.document.get_users_docs import get_users_documents as service
 from utils.auth import get_current_user
 
 
@@ -14,10 +18,28 @@ router = APIRouter()
     summary="현재 사용자의 문서 조회",
     tags=["문서"]
 )
-def get_user_documents(
-    user: Annotated[User, Security(get_current_user)]
-) -> list[DocumentRead]:
-    return [
+async def get_users_documents(
+    pagination: Annotated[PaginationParams, Query()],
+    user: Annotated[User, Security(get_current_user)],
+    session: Annotated[AsyncSession, Depends(get_async_db)]
+) -> PaginationResponse[DocumentRead]:
+    offset = (pagination.page - 1) * pagination.size
+    limit = pagination.size
+
+    data = await service(
+        user=user,
+        session=session,
+        offset=offset,
+        limit=limit
+    )
+
+    response_data = [
         DocumentRead.model_validate(doc)
-        for doc in user.uploaded_documents
+        for doc in data
     ]
+
+    return PaginationResponse(
+        page=pagination.page,
+        size=pagination.size,
+        data=response_data
+    )
