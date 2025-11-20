67fb1f4c3641e2e2cf23cad4ef1808a8c09135dc
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-20 14:08:16 +0900

    유저에 소속된 chat space 조회 API 엔드포인트 추가
    `GET /v1/users/me/spaces`

---
+++ app/api/v1/users/me/get_user_space.py
@@ -0,0 +1,43 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query, Security
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.session import get_async_db
+from errors.general import IllegalStateError
+from models import User
+from schemas.chat import SpaceRead
+from schemas.pagination import PaginationParams, PaginationResponse
+from services.users.get_user_space import get_user_space as service
+from utils.auth import get_current_user
+
+
+router = APIRouter()
+
+@router.get("/spaces", summary="사용자의 스페이스 조회")
+async def get_users_space(
+    pagination: Annotated[PaginationParams, Query()],
+    user: Annotated[User, Security(get_current_user)],
+    session: Annotated[AsyncSession, Depends(get_async_db)]
+) -> PaginationResponse[SpaceRead]:
+    if user.user_id is None:
+        raise IllegalStateError()
+
+    offset = (pagination.page - 1) * pagination.size
+    limit = pagination.size
+
+    data = await service(
+        user=user,
+        session=session,
+        offset=offset,
+        limit=limit
+    )
+
+    return PaginationResponse(
+        page=pagination.page,
+        size=pagination.size,
+        data=[
+            SpaceRead.model_validate(d)
+            for d in data
+        ],
+    )
