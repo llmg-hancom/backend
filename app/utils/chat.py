7b3a90e484753ace3dda6ca72e4a584ac82866eb
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-21 10:06:57 +0900

    URL Path의 `{space_id}`를 통해 ChatSpace 객체를 불러오는 의존성 추가

---
+++ app/utils/chat.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends, Path
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
+
+from db.session import get_async_db
+from errors.space import SpaceNotFoundError
+from models import ChatSpace
+
+
+async def chat_space_from_space_id_path(
+    space_id: Annotated[int, Path()],
+    session: Annotated[AsyncSession, Depends(get_async_db)]
+) -> ChatSpace:
+    query = (select(ChatSpace).where(ChatSpace.space_id == space_id))
+    result = await session.scalar(query)
+
+    if result is None:
+        raise SpaceNotFoundError(space_id=space_id)
+
+    return result
