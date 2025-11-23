12f9e005569dfd2594debef569ab65b1833d92cc
hoyneed <hoyneed@gmail.com>
2025-11-23 19:56:18 +0900

    [RAG] developing RAG RAG 개발 중

--- app/rag/context_manager.py
+++ app/rag/context_manager.py
@@ -1,11 +1,9 @@
-# --- DB 세션 관리를 위한 비동기 컨텍스트 매니저 ---
 from contextlib import asynccontextmanager
-
 from sqlmodel.ext.asyncio.session import AsyncSession
-
 from db.session import async_engine
 
 
+# --- DB 세션 관리를 위한 비동기 컨텍스트 매니저 ---
 @asynccontextmanager
 async def get_db_session():
     async with AsyncSession(async_engine) as session:
