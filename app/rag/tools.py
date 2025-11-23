643a692528e743351b9adcfdf34141e6168d099a
hoyneed <hoyneed@gmail.com>
2025-11-23 12:27:47 +0900

    [CD/CI] workflow, celery beat edit celery beat, workflow 정리 중

--- app/rag/tools.py
+++ app/rag/tools.py
@@ -3,7 +3,7 @@ from langchain_postgres import PGEngine, PGVectorStore
 from sqlmodel import select
 
 from db.session import async_engine
-from models import Document
+from models import Document, ChatSpaceDocument
 from models.document import DocumentScope
 from rag.context_manager import get_db_session
 from rag.model import embeddings
@@ -34,17 +34,82 @@ async def search_public_law(query: str):
 
     """
     vector_store = await create_vector_store()
+    relevant_chunks = []
+    serialized = ""
     async with get_db_session() as session:
         statement = select(Document.document_id).where(
             Document.document_scope == DocumentScope.public_law
         )
         results = await session.execute(statement)
         target_doc_ids = results.scalars().all()
-        relevent_chunks = await vector_store.asimilarity_search(
+    if target_doc_ids:
+        relevant_chunks = await vector_store.asimilarity_search(
             query, k=5, filter={"document_id": {"$in": target_doc_ids}}
         )
         serialized = "\n\n".join(
             f"Source: {doc.metadata}\nContent: {doc.page_content}"
-            for doc in relevent_chunks
+            for doc in relevant_chunks
         )
-        return serialized, relevent_chunks
+
+    return serialized, relevant_chunks
+
+
+@tool(response_format="content_and_artifact")
+async def search_precedent(query: str):
+    """
+    This tool is designed for RAG in LLMs,
+    specifically for searching Korean precedents.
+
+    Args:
+        query (str): The search query for precedents.
+
+    """
+    vector_store = await create_vector_store()
+    relevant_chunks = []
+    serialized = ""
+    async with get_db_session() as session:
+        statement = select(Document.document_id).where(
+            Document.document_scope == DocumentScope.precedent
+        )
+        results = await session.execute(statement)
+        target_doc_ids = results.scalars().all()
+    if target_doc_ids:
+        relevant_chunks = await vector_store.asimilarity_search(
+            query, k=5, filter={"document_id": {"$in": target_doc_ids}}
+        )
+        serialized = "\n\n".join(
+            f"Source: {doc.metadata}\nContent: {doc.page_content}"
+            for doc in relevant_chunks
+        )
+    return serialized, relevant_chunks
+
+
+@tool(response_format="content_and_artifact")
+async def search_private_document(space_id: int, query: str):
+    """
+    This tool is designed for RAG in LLMs,
+    specifically for searching private document chunks.
+
+    Args:
+        space_id (int): The ID of the chat space.
+        query (str): The search query for precedents.
+
+    """
+    vector_store = await create_vector_store()
+    relevant_chunks = []
+    serialized = ""
+    async with get_db_session() as session:
+        statement = select(ChatSpaceDocument.space_document_id).where(
+            ChatSpaceDocument.space_id == space_id
+        )
+        results = await session.execute(statement)
+        target_doc_ids = results.scalars().all()
+    if target_doc_ids:
+        relevant_chunks = await vector_store.asimilarity_search(
+            query, k=5, filter={"document_id": {"$in": target_doc_ids}}
+        )
+        serialized = "\n\n".join(
+            f"Source: {doc.metadata}\nContent: {doc.page_content}"
+            for doc in relevant_chunks
+        )
+    return serialized, relevant_chunks
