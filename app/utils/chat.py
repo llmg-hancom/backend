56b7150d06821649d96a068585d4b2a50edaf151
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-25 11:16:38 +0900

    URL 경로에 포함된 `{session_id}`를 통해 ChatSession 객체를 반환하는 `chat_session_from_session_id_path` 함수 추가

--- app/utils/chat.py
+++ app/utils/chat.py
@@ -5,8 +5,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import col, select
 
 from db.session import get_async_db
+from errors.chat import ChatSessionNotFoundError
 from errors.space import SpaceNotFoundError
-from models import ChatSpace
+from models import ChatSession, ChatSpace
 
 
 async def chat_space_from_space_id_path(
@@ -23,5 +24,18 @@ async def chat_space_from_space_id_path(
 
     if result is None:
         raise SpaceNotFoundError(space_id=space_id)
+
+    return result
+
+
+async def chat_session_from_session_id_path(
+    session_id: Annotated[int, Path(description="채팅 세션 ID")],
+    db: Annotated[AsyncSession, Depends(get_async_db)],
+) -> ChatSession:
+    query = (select(ChatSession).where(ChatSession.session_id == session_id))
+    result = await db.scalar(query)
+
+    if result is None:
+        raise ChatSessionNotFoundError(session_id=session_id)
 
     return result
