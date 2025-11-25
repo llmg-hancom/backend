f7e83aafb7cde8543186b654f3a0c520e6575fda
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-25 14:39:53 +0900

    그룹 챗스페이스의 목록을 조회하는 API 엔드포인트 구현
    `GET /v1/groups/{group_id}/spaces`

--- app/api/v1/groups/group_chat_space.py
+++ app/api/v1/groups/group_chat_space.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Body, Depends, status
+from fastapi import APIRouter, Body, Depends, Query, status
 
 from models import Group
 from schemas.chat import SpaceCreateRequest, SpaceRead
+from schemas.pagination import PaginationParams, PaginationResponse
 from services.chat_service import ChatService
 from utils.group import get_group_from_group_id_path
 
@@ -25,3 +26,28 @@ async def create_space(
 ) -> SpaceRead:
     space = await service.create_group_chat_space(group, body.name)
     return SpaceRead.model_validate(space)
+
+
+@router.get(
+    "",
+    summary="그룹 챗스페이스 목록 조회",
+)
+async def get_spaces(
+    group: Annotated[Group, Depends(get_group_from_group_id_path)],
+    page: Annotated[PaginationParams, Query()],
+    service: Annotated[ChatService, Depends(ChatService.factory)]
+) -> PaginationResponse[SpaceRead]:
+    offset = (page.page - 1) * page.size
+    limit = page.size
+
+    spaces = await service.get_group_chat_spaces(
+        group=group,
+        offset=offset,
+        limit=limit,
+    )
+
+    return PaginationResponse(
+        page=page.page,
+        size=page.size,
+        data=[SpaceRead.model_validate(space) for space in spaces]
+    )
