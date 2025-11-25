9e529979f50c9d2b4c11195f909f83dd26b93fec
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-25 14:17:32 +0900

    그룹 챗스페이스 생성 기능 추가
    `POST /v1/groups/{group_id}/spaces`

--- app/api/v1/groups/__init__.py
+++ app/api/v1/groups/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .create_group import router as create_group_router
+from .group_chat_space import router as group_chat_space_router
 from .group_invite import router as invite_router
 from .my_group import router as my_group_router
 from .remove_group_member import router as remove_group_member_router
@@ -12,3 +13,4 @@ router.include_router(create_group_router, tags=["그룹"])
 router.include_router(my_group_router, tags=["그룹"])
 router.include_router(invite_router, prefix="/groups", tags=["그룹"])
 router.include_router(remove_group_member_router, prefix="/groups", tags=["그룹"])
+router.include_router(group_chat_space_router, prefix="/groups", tags=["그룹", "채팅"])
