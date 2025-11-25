fb0b5a66f896e411f0e388fabb42a43bc35ebfaf
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-11-25 11:15:02 +0900

    `chat_space_from_space_id_path` 함수가 삭제된 (`deleted_at`이 있는) space를 찾는 문제 수정

--- app/utils/chat.py
+++ app/utils/chat.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends, Path
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlmodel import select
+from sqlmodel import col, select
 
 from db.session import get_async_db
 from errors.space import SpaceNotFoundError
@@ -13,7 +13,12 @@ async def chat_space_from_space_id_path(
     space_id: Annotated[int, Path()],
     session: Annotated[AsyncSession, Depends(get_async_db)]
 ) -> ChatSpace:
-    query = (select(ChatSpace).where(ChatSpace.space_id == space_id))
+    query = (
+        select(ChatSpace)
+        .where(ChatSpace.space_id == space_id)
+        .where(col(ChatSpace.deleted_at).is_(None))
+    )
+
     result = await session.scalar(query)
 
     if result is None:
