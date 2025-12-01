25271e5c3dd44ced548bc565a92f8774c74f2687
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-12-01 13:43:09 +0900

    그룹 정보 조회 API 구현 (`GET /v1/groups/{group_id}`)

--- app/api/v1/groups/__init__.py
+++ app/api/v1/groups/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .create_group import router as create_group_router
+from .get_group_info import router as get_group_info_router
 from .group_chat_space import router as group_chat_space_router
 from .group_invite import router as invite_router
 from .my_group import router as my_group_router
@@ -14,3 +15,4 @@ router.include_router(my_group_router, tags=["그룹"])
 router.include_router(invite_router, prefix="/groups", tags=["그룹"])
 router.include_router(remove_group_member_router, prefix="/groups", tags=["그룹"])
 router.include_router(group_chat_space_router, prefix="/groups", tags=["그룹", "채팅"])
+router.include_router(get_group_info_router, prefix="/groups", tags=["그룹"])
