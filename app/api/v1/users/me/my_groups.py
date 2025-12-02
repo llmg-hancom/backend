e7a3abe0cb9b8049a63e0e4311abbae7d834636a
hazelnut4523 <lee.gyeongmuk@icloud.com>
2025-12-02 10:36:05 +0900

    내 그룹 목록을 조회하는 API에서 삭제된(`deleted_at`이 있는) 그룹을 조회하지 않도록 변경, pagination 적용

--- app/api/v1/users/me/my_groups.py
+++ app/api/v1/users/me/my_groups.py
@@ -1,9 +1,13 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Security
+from fastapi import APIRouter, Depends, Query, Security
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from db.session import get_async_db
 from models.user import User, UserRead
 from schemas.groups import GroupReadWithoutMembers
+from schemas.pagination import PaginationParams, PaginationResponse
+from services.users.get_my_group import get_my_group as service
 from utils.auth import get_current_user
 
 
@@ -14,16 +18,23 @@ router = APIRouter()
     summary="현재 사용자가 소속된 그룹 조회",
     tags=["그룹"]
 )
-def my_groups(
+async def my_groups(
     user: Annotated[User, Security(get_current_user)],
-) -> list[GroupReadWithoutMembers]:
-    return [
-        GroupReadWithoutMembers(
-            group_id=group.group.group_id,
-            group_name=group.group.group_name,
-            created_at=group.group.created_at,
-            created_by_user=UserRead.model_validate(group.group.created_by_user),
-        )
-        for group in user.group_memberships
-        if group.group.group_id is not None
-    ]
+    db: Annotated[AsyncSession, Depends(get_async_db)],
+    pagination: Annotated[PaginationParams, Query()]
+) -> PaginationResponse[GroupReadWithoutMembers]:
+    offset = (pagination.page - 1) * pagination.size
+    limit = pagination.size
+
+    response = await service(
+        actor=user,
+        db=db,
+        offset=offset,
+        limit=limit
+    )
+
+    return PaginationResponse(
+        page=pagination.page,
+        size=pagination.size,
+        data=[GroupReadWithoutMembers.model_validate(group) for group in response]
+    )
