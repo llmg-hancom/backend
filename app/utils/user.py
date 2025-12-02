0ae199b89cf849815de7625648138f045c00bcb6
hoyneed <hoyneed@gmail.com>
2025-12-03 00:29:46 +0900

    [RAG] adding law search methods 법령을 법령명/ 조 번호로 검색 기능 추가 중

--- app/utils/user.py
+++ app/utils/user.py
@@ -1,8 +1,8 @@
 from typing import Annotated, Literal
 
 from fastapi import Depends, Path, Security
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from db.session import get_async_db
 from errors.auth import UserNotFoundError
@@ -14,8 +14,8 @@ from utils.auth import get_current_user
 
 async def get_user_id_from_path(
     user_id: Annotated[int | Literal["me"], Path()],
-    current_user: Annotated[User, Security(get_current_user)]
-) -> int:
+    current_user: Annotated[User, Security(get_current_user)],
+) -> int | Literal["me"]:
     if current_user.user_id is None:
         raise IllegalStateError()
 
@@ -25,13 +25,13 @@ async def get_user_id_from_path(
 async def get_user_from_user_id_path(
     user_id: Annotated[int, Depends(get_user_id_from_path)],
     current_user: Annotated[User, Security(get_current_user)],
-    session: Annotated[AsyncSession, Depends(get_async_db)]
+    session: Annotated[AsyncSession, Depends(get_async_db)],
 ) -> User:
     # 요청한 user_id와 인증된 사용자의 user_id가 다를 때
     if user_id != current_user.user_id:
         raise UserForbidden()
 
-    query = (select(User).where(User.user_id == user_id))
+    query = select(User).where(User.user_id == user_id)
     user = await session.scalar(query)
 
     if user is None:
