9abe9a248033cf76fb9e5a65af71cb79a0ef9e08
hoyneed <hoyneed@gmail.com>
2025-12-03 22:55:36 +0900

    [feature] include user role in member read 그룹 멤버 조회할 때 user role도 포함. 필요없는 model_validate 제거

--- app/services/group/get_group_members.py
+++ app/services/group/get_group_members.py
@@ -1,8 +1,9 @@
 from pydantic import PositiveInt
+from sqlalchemy.orm import joinedload
 from sqlmodel import col, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from models import Group, GroupMember, User
+from models import Group, GroupMember
 from schemas.groups import GroupMemberRead
 
 
@@ -13,12 +14,11 @@ async def get_group_members(
     limit: PositiveInt,
 ) -> list[GroupMemberRead]:
     query = (
-        select(User, GroupMember.role)
-        .join(GroupMember)
+        select(GroupMember)
         .where(col(GroupMember.group_id) == group.group_id)
         .offset(offset)
         .limit(limit)
+        .options(joinedload(GroupMember.user))
     )
     result = await db.exec(query)
-    members = [GroupMemberRead.model_validate(user) for user in result.all()]
-    return members
+    return result.all()
