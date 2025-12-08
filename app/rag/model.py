11f8a11c2ac08f86c7c2966082788799fb2d1030
hoyneed <hoyneed@gmail.com>
2025-12-08 17:28:08 +0900

    [model] gemini-3 gemini 3 test

--- app/rag/model.py
+++ app/rag/model.py
@@ -4,7 +4,7 @@ from core.config import settings
 
 
 llm = ChatOllama(
-    model="gpt-oss:latest",
+    model="gemini-3-pro-preview:latest",
     validate_model_on_init=True,
     base_url=settings.OLLAMA_BASE_URL,
     temperature=0.2,
