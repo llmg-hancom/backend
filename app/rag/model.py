8ccfc9e5f6f76e6b0e298591a9295029a559d2e9
hoyneed <hoyneed@gmail.com>
2025-12-08 17:41:45 +0900

    [model] qwen3 qwen3 test

--- app/rag/model.py
+++ app/rag/model.py
@@ -4,7 +4,7 @@ from core.config import settings
 
 
 llm = ChatOllama(
-    model="qwen3:30b-instruct",
+    model="gpt-oss:latest",
     validate_model_on_init=True,
     base_url=settings.OLLAMA_BASE_URL,
     temperature=0.2,
